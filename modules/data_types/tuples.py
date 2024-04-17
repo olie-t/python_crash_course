389ae574669e496f69c7d0d9d5fcde3947a1ced5
Olie <olthomas13@gmail.com>
2024-04-17 13:38:35 +0100

    Working through first chapters

---
+++ modules/data_types/tuples.py
@@ -0,0 +1,14 @@
+# Think of 5 simple foods and store them in a tuple
+foods: tuple = ("Chips", "Boiled Eggs", "Chicken Thighs", "Leaf Salad", "Pickles")
+
+# Use a for loop to print the foods
+
+for food in foods:
+    print(food)
+
+# we are now offering some different foods, redine the tuple and print it
+
+foods: tuple = ("Chips", "Baked Beans", "Chicken Thighs", "Chop Salad", "Pickles")
+
+for food in foods:
+    print(food)
