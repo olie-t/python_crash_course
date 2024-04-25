031a38d2da6e303edc00094f825b988b5e241473
Olie <olthomas13@gmail.com>
2024-04-25 09:00:22 +0100

    Working through first chapters

---
+++ modules/if_statements/conditional_test.py
@@ -0,0 +1,14 @@
+str_test:str = "String"
+print("string".title() == str_test)
+print("string" == str_test.lower())
+print("string" == str_test)
+print(4 > 5)
+print(4 == 2 + 2)
+print(True or False)
+print(True and False)
+print(4 == 2+2 and True)
+test_list: list = [1,2,3,4,5,6]
+print(5 in test_list)
+print(20 in test_list)
+print(False != (20 not in test_list))
+
---
+++ modules/if_statements/conditional_test_2.py
@@ -0,0 +1,34 @@
+alien_colour = 'green'
+if alien_colour == 'green':
+    print('You got 5 pts')
+
+if alien_colour == 'yellow':
+    pass
+
+if alien_colour == 'green':
+    print('You got 5 pts')
+
+if alien_colour != 'green':
+    print('You got 10 pts')
+
+if alien_colour == 'green':
+    print('You got 5 pts')
+elif alien_colour == 'yellow':
+    print('You got 10 pts')
+else:
+    print('You got 15 pts')
+
+alien_colour = 'yellow'
+if alien_colour == 'green':
+    print('You got 5 pts')
+elif alien_colour == 'yellow':
+    print('You got 10 pts')
+else:
+    print('You got 15 pts')
+alien_colour = 'red'
+if alien_colour == 'green':
+    print('You got 5 pts')
+elif alien_colour == 'yellow':
+    print('You got 10 pts')
+else:
+    print('You got 15 pts')
