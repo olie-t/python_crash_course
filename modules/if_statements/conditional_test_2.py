24a42c6d8cf5c04500bcc3c5b57968ee6c3f309e
Olie <olthomas13@gmail.com>
2024-04-26 13:41:42 +0100

    Working through first chapters

--- modules/if_statements/conditional_test_2.py
+++ modules/if_statements/conditional_test_2.py
@@ -32,3 +32,23 @@ elif alien_colour == 'yellow':
     print('You got 10 pts')
 else:
     print('You got 15 pts')
+
+age:int = int(input("What is your age?"))
+
+if age < 2:
+    print("You are a baby")
+if 2 <= age < 4:
+    print("You are a toddler")
+if 4 <= age < 13:
+    print("You are a child")
+if 13 <= age < 20:
+    print("You are a teenager")
+if 20 <= age < 65:
+    print("You are an adult")
+if age > 65:
+    print("You are an elder")
+
+fav_fruits = ["Banana", "Apple", "Orange", "Melon", "Pineapple", "Grapes"]
+
+if "Banana" in fav_fruits:
+    print("Bananas are great")
